09879f8ff1f42fdea41a2bd9d85915b6f90bc050
Xurshid <globalcrmglobal@gmail.com>
2025-01-07 20:40:05 +0500

    xatolik tuzatildi

--- app/Http/Controllers/BreakLogsController.php
+++ app/Http/Controllers/BreakLogsController.php
@@ -174,12 +174,18 @@ class BreakLogsController extends Controller
         foreach($unuse_break_logs as $unuse_break_log){
             $unuse_break_log->delete();
         }
-
-        $too_long_break_logs = BreakLogs::where('break_start', '<', date('Y-m-d H:i:s', strtotime('-4 hours')))->get();
-        foreach($too_long_break_logs as $too_long_break_log){
-            $too_long_break_log->delete();
+        $started_break_logs = BreakLogs::whereNotNull('break_start')->where('break_start', '<', date('Y-m-d H:i:s', strtotime('-4 hours')))->get();
+        foreach($started_break_logs as $started_break_log){
+            if(!$started_break_log->getBreakEnd){
+                $started_break_log->delete();
+            }
         }
-
+        $ended_break_logs = BreakLogs::whereNotNull('break_end')->get();
+        foreach($ended_break_logs as $ended_break_log){
+            if(!$ended_break_log->getBreakStart){
+                $ended_break_log->delete();
+            }
+        }
         $status_activate_text = translate_title('Tanaffusni boshlash');
         $status_disactivate_text = translate_title('Tanaffusni tugatish');
         $user_self = [

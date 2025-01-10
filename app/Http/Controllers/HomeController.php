4dc73e0f497e4922094bed61b98ddd05082436a7
Xurshid <globalcrmglobal@gmail.com>
2025-01-10 21:04:31 +0500

    vaqtni formati to'g'rilandi

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -292,6 +292,15 @@ class HomeController extends Controller
                 }
             }
         }
+        if($hour>10){
+            $hour = '0'.$hour;
+        }
+        if($minute>10){
+            $minute = '0'.$minute;
+        }
+        if($second>10){
+            $second = '0'.$second;
+        }
         return $hour.':'.$minute.':'.$second;
     }
 
